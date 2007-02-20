boo/boo-lang

commit 9124b0def97b6dc7af35df22c7896461227a5f92

generics patch by Avish fixing BOO-755, BOO-756 and BOO-757
git-svn-id: https://svn.codehaus.org/boo/boo/trunk@2337 2c1201b4-01cd-e047-a400-b836ae1fbc61

src/Boo.Lang.Compiler/Boo.Lang.Compiler.csproj
@@ -27,7 +27,7 @@
     <Optimize>false</Optimize>
     <AllowUnsafeBlocks>false</AllowUnsafeBlocks>
     <CheckForOverflowUnderflow>false</CheckForOverflowUnderflow>
-    <DefineConstants>TRACE;DEBUG;IGNOREKEYFILE NET_2_0</DefineConstants>
+    <DefineConstants>TRACE;DEBUG;IGNOREKEYFILE;NET_2_0</DefineConstants>
     <OutputPath>..\..\ide-build</OutputPath>
     <TreatWarningsAsErrors>false</TreatWarningsAsErrors>
   </PropertyGroup>
@@ -445,6 +445,7 @@
     <Compile Include="Util\TaskList.cs" />
     <Compile Include="Steps\CheckIdentifiers.cs" />
     <Compile Include="Steps\OptimizeIterationStatements.cs" />
+    <Compile Include="TypeSystem\ExternalGenericType.cs" />
   </ItemGroup>
   <ItemGroup>
     <ProjectReference Include="..\Boo.Lang\Boo.Lang.csproj">

src/Boo.Lang.Compiler/Steps/BindBaseTypes.cs
@@ -128,26 +128,21 @@ namespace Boo.Lang.Compiler.Steps
 			foreach (SimpleTypeReference type in node.BaseTypes.ToArray())
 			{
 				NameResolutionService.ResolveSimpleTypeReference(type);
-				IType entity = type.Entity as IType;
-				
-				if (null != entity)
+
+				AbstractInternalType internalType = type.Entity as AbstractInternalType;
+				if (null != internalType)
 				{
-					AbstractInternalType internalType = entity as AbstractInternalType;
-					if (null != internalType)
+					if (visited.Contains(internalType.TypeDefinition))
 					{
-						if (visited.Contains(internalType.TypeDefinition))
-						{
-							Error(CompilerErrorFactory.InheritanceCycle(type, internalType.FullName));
-							node.BaseTypes.RemoveAt(index-removed);
-							++removed;
-						}
-						else
-						{
-							ResolveBaseTypes(visited, internalType.TypeDefinition);
-						}
+						Error(CompilerErrorFactory.InheritanceCycle(type, internalType.FullName));
+						node.BaseTypes.RemoveAt(index-removed);
+						++removed;
+					}
+					else
+					{
+						ResolveBaseTypes(visited, internalType.TypeDefinition);
 					}
 				}
-				
 				++index;
 			}
 		}

src/Boo.Lang.Compiler/Steps/EmitAssembly.cs
@@ -2575,6 +2575,25 @@ namespace Boo.Lang.Compiler.Steps
 			}
 		}
 		
+		override public void OnGenericReferenceExpression(GenericReferenceExpression node)
+		{
+			IEntity tag = TypeSystem.TypeSystemServices.GetEntity(node);
+			switch (tag.EntityType)
+			{
+				case EntityType.Type:
+				{
+					EmitGetTypeFromHandle(GetSystemType(node));
+					break;
+				}
+
+				default:
+				{
+					NotImplemented(node, tag.ToString());
+					break;
+				}
+			}
+		}
+		
 		override public void OnMemberReferenceExpression(MemberReferenceExpression node)
 		{
 			IEntity tag = TypeSystem.TypeSystemServices.GetEntity(node);

src/Boo.Lang.Compiler/Steps/ProcessMethodBodies.cs
@@ -2036,6 +2036,7 @@ namespace Boo.Lang.Compiler.Steps
 			IType genericType = typeDef.MakeGenericType(GetGenericArguments(node));
 			Bind(node, genericType);
 			BindExpressionType(node, this.TypeSystemServices.TypeType);
+			
 		}
 
 		private IType[] GetGenericArguments(GenericReferenceExpression node)

src/Boo.Lang.Compiler/TypeSystem/AbstractInternalType.cs
@@ -230,8 +230,8 @@ namespace Boo.Lang.Compiler.TypeSystem
 					}
 				}
 			}
-                        if (_typeDefinition.BaseTypes.Count > 0)
-                        {
+	        if (_typeDefinition.BaseTypes.Count > 0)
+	        {
 				List buffer = new List();
 				
 				foreach (TypeReference baseType in _typeDefinition.BaseTypes)

src/Boo.Lang.Compiler/TypeSystem/ExternalGenericType.cs
@@ -26,57 +26,64 @@
 // THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion
 
-namespace Boo.Lang.Compiler.Steps
+namespace Boo.Lang.Compiler.TypeSystem
 {
-	using Boo.Lang.Compiler.Ast;
-	using Boo.Lang.Compiler.TypeSystem;
-	
-	public class ResolveTypeReferences : AbstractNamespaceSensitiveVisitorCompilerStep
+	using System;
+
+	public class ExternalGenericType : ExternalType, IGenericType
 	{
-		public ResolveTypeReferences()
-		{
-		}
-
-		override public void Run()
-		{
-			Visit(CompileUnit.Modules);
-		}
-
-		override public void OnArrayTypeReference(ArrayTypeReference node)
-		{
-			NameResolutionService.ResolveArrayTypeReference(node);
-		}
+		IType[] _arguments = null;
+		bool _constructed;
 		
-		override public void OnSimpleTypeReference(SimpleTypeReference node)
+#if NET_2_0
+		public ExternalGenericType(TypeSystemServices tss, Type type) : base(tss, type)
 		{
-			NameResolutionService.ResolveSimpleTypeReference(node);
-		}
+			_constructed = !type.ContainsGenericParameters;
+		}		
 
-		override public void OnGenericTypeReference(GenericTypeReference node)
+		public IType[] GetGenericArguments()
 		{
-			NameResolutionService.ResolveSimpleTypeReference(node);
-		}
-		
-		override public void LeaveCallableTypeReference(CallableTypeReference node)
-		{
-			IParameter[] parameters = new IParameter[node.Parameters.Count];
-			for (int i=0; i<parameters.Length; ++i)
+			if (_arguments == null)
 			{
-				parameters[i] = new InternalParameter(node.Parameters[i], i);
+				Type[] actualArguments = ActualType.GetGenericArguments();
+				_arguments = new IType[actualArguments.Length];
+				
+				Converter<Type, IType> map = _typeSystemServices.Map;
+				_arguments = Array.ConvertAll(actualArguments, map);
 			}
 			
-			IType returnType = null;
-			if (null != node.ReturnType)
-			{
-				returnType = GetType(node.ReturnType);
-			}
-			else
-			{
-				returnType = TypeSystemServices.VoidType;
-			}
-			
-			node.Entity = TypeSystemServices.GetConcreteCallableType(node, new CallableSignature(parameters, returnType));
+			return _arguments;
 		}
+		
+		public IGenericTypeDefinition GetGenericTypeDefinition()
+		{
+			return (IGenericTypeDefinition)_typeSystemServices.Map(ActualType.GetGenericTypeDefinition());
+		}
+		
+		public bool FullyConstructed
+		{
+			get { return _constructed; }
+		}		
 
+#else
+		public ExternalGenericType(TypeSystemServices tss, Type type) : base(tss, type)
+		{
+		}		
+
+		IType[] IGenericType.GetGenericArguments()
+		{
+			throw new NotImplementedException();
+		}
+		
+		IGenericType IGenericConstructedType.GetGenericTypeDefinition()
+		{
+			throw new NotImplementedException();
+		}
+		
+		bool IGenericType.FullyConstructed
+		{
+			get { throw new NotImplementedException(); }
+		}
+#endif
 	}
 }

src/Boo.Lang.Compiler/TypeSystem/ExternalGenericTypeDefinition.cs
@@ -75,33 +75,28 @@ namespace Boo.Lang.Compiler.TypeSystem
 			IGenericParameter[] parameters = new IGenericParameter[arguments.Length];
 			for (int i=0; i<arguments.Length; ++i)
 			{
-				parameters[i] = new ExternalGenericParameter(arguments[i]);
+				parameters[i] = new ExternalGenericParameter(_typeSystemServices, arguments[i]);
 			}
 			return parameters;
 		}
 
-		class ExternalGenericParameter : IGenericParameter
+		public class ExternalGenericParameter : ExternalType, IGenericParameter
 		{
-			Type _type;
+			bool _constructed;
 			
-			public ExternalGenericParameter(Type type)
-			{	
-				_type = type;
+			public ExternalGenericParameter(TypeSystemServices tss, Type type) : base(tss, type)
+			{					
+				_constructed = type.IsGenericParameter;
 			}
 			
-			public string Name
+			public IGenericTypeDefinition GetDeclaringType()
 			{
-				get { return _type.Name; }
+				return (IGenericTypeDefinition)_typeSystemServices.Map(ActualType.DeclaringType);
 			}
-
-			public string FullName
+			
+			public bool Constructed
 			{
-				get { return _type.FullName; }
-			}
-
-			public EntityType EntityType
-			{
-				get { return TypeSystem.EntityType.GenericParameter; }
+				get { return _constructed; }
 			}
 		}
 	}

src/Boo.Lang.Compiler/TypeSystem/IEntity.cs
@@ -194,14 +194,23 @@ namespace Boo.Lang.Compiler.TypeSystem
 		bool IsAssignableFrom(IType other);
 	}
 	
-	public interface IGenericParameter : IEntity
-	{	
+	public interface IGenericParameter : IType
+	{
+		IGenericTypeDefinition GetDeclaringType();
+		bool Constructed { get; }
 	}
 	
 	public interface IGenericTypeDefinition : IType
 	{
 		IGenericParameter[] GetGenericParameters();
 		IType MakeGenericType(IType[] arguments);
+	}
+	
+	public interface IGenericType : IType
+	{
+		IType[] GetGenericArguments();
+		IGenericTypeDefinition GetGenericTypeDefinition();
+		bool FullyConstructed { get; }
 	}
 	
 	public interface ICallableType : IType

commit 646b3581f31c68408103327bbefa7616c1cbc923

- added testcase for the regression bug in r2337 : the "CompilerGeneratedExtensionsModule" Ast.Module is created with a null name, this then breaks verbose compile (-vv/-vvv) by throwing a NullReferenceException when then ToString() is used (by string.Format in *Trace methods). I'm not commiting a fix because I'm not sure where it is best to put it. - updated AbstractCompilerTestCase, forgotten to commit it in the last commit adding verify pipeline support to Mono.
git-svn-id: https://svn.codehaus.org/boo/boo/trunk@2410 2c1201b4-01cd-e047-a400-b836ae1fbc61

tests/Boo.Lang.Compiler.Tests/BooCompilerTestFixture.boo
@@ -110,7 +110,7 @@ class CompilerTestFixture:
 		_compiler.Parameters.OutputAssembly = fname = Path.Combine(Path.GetTempPath(), "foo.exe")
 		
 		context = _compiler.Run()
-		Assert.AreEqual(0, len(context.Errors))
+		Assert.AreEqual(0, len(context.Errors), "context.Errors")
 		assert File.Exists(fname)
 		assert context.GeneratedAssembly is null 
 		
@@ -128,3 +128,10 @@ class CompilerTestFixture:
 			Console.SetOut(saved)
 		Assert.AreEqual("foo", writer.ToString().Trim())
 
+
+	[Test]
+	def VerboseCompile():
+		_compiler.Parameters.TraceSwitch.Level = System.Diagnostics.TraceLevel.Verbose; 
+		CompileOnlyToFile()
+		
+		

tests/BooCompiler.Tests/AbstractCompilerTestCase.cs
@@ -56,8 +56,7 @@ namespace BooCompiler.Tests
 #if VISUAL_STUDIO
 				return false;
 #else
-				return Boo.Lang.Compiler.Steps.PEVerify.IsSupported &&
-						GetEnvironmentFlag("peverify", true);
+				return GetEnvironmentFlag("peverify", true);
 #endif
 			}
 		}